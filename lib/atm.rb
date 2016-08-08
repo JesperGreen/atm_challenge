6cc8b6b5ab5921e5a1613e242f7f38cb6bf82e70
Jesper Green Stromblad <jesper@Jespers-MacBook-Pro.local>
2016-08-08 16:56:04 -0700

    abstracting the withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,7 +1,28 @@
 class Atm
-  attr_accessor :funds
+  attr_accessor :balance
 
   def initialize
-    @funds = 1000
+    @balance = 1000
+  end
+
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account) then
+      return
+    else
+      perform_transaction(amount, account)
+    end
+  end
+
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @balance -= amount
+    account.balance -= amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
   end
 end
