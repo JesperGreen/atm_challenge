8df619b4566e205b3717619a7c45f6446d18c08f
Jesper Green Stromblad <jesper@Jespers-MacBook-Pro.local>
2016-08-09 14:41:38 -0700

    setting up - first success

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,43 +1,7 @@
 class Atm
-  attr_accessor :balance
+  attr_accessor :funds
 
   def initialize
-    @balance = 1000
-  end
-
-  def withdraw(amount, pin_code, account)
-    case
-    when insufficient_funds_in_account?(amount, account) then
-      { status: false, message: 'insufficient funds in account', date: Date.today }
-    when insufficient_funds_in_atm?(amount) then
-      { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    when incorrect_pin?(pin_code, account.pin_code) then
-      { status: false, message: 'wrong pin code', date: Date.today }
-    else
-      perform_transaction(amount, account)
-    end
-  end
-
-  def perform_transaction(amount, account)
-    @balance -= amount
-    account.balance -= amount
-    { status: true, message: 'success', date: Date.today, amount: amount }
-  end
-
-  #private methods
-
-  private
-
-  def insufficient_funds_in_account?(amount, account)
-    amount > account.balance
-  end
-
-
-  def insufficient_funds_in_atm?(amount)
-    @balance < amount
-  end
-
-  def incorrect_pin?(pin_code, actual_pin)
-    pin_code != actual_pin
+    @funds = 1000
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,41 +1,7 @@
 require './lib/atm.rb'
-require 'date'
 
 describe Atm do
-  let(:account) { class_double('Account') }
-
-  before do
-    allow(account).to receive(:balance).and_return(100)
-    allow(account).to receive(:balance=)
-  end
-
-  it 'has 1000$ on intitialize' do
-    expect(subject.balance).to eq 1000
-  end
-
-  it 'funds are reduced at withdraw' do
-    subject.withdraw(50, '1234', account)
-    expect(subject.balance).to eq 950
-  end
-
-  it 'allow withdraw if account has enough balance' do
-    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-    expect(subject.withdraw(45, '1234', account)).to eq expected_output
-  end
-
-  it 'reject withdraw if account has insufficient funds' do
-    expected_output = { status: true, message: 'insufficient funds', date: Date.today }
-    expect(subject.withdraw(105, '1234', account)).to eq expected_output
-  end
-
-  it 'reject withdraw if ATM has insufficient funds' do
-    subject.balance = 50
-    expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    expect(subject.withdraw(100, '1234', account)).to eq expected_output
-  end
-
-  it 'reject withdraw if pin is wrong' do
-    expected_output = { status: false, message: 'wrong pin code' date: Date.today }
-    expect(subject.withdraw(50, 9999, account)).to eq expected_output
+  it 'has 1000$ on initialize' do
+    expect(subject.funds).to eq 1000
   end
 end
