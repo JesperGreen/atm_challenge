66db3eaaa107dd64dd2207e9217c5508fb94cf78
Jesper Green Stromblad <jesper@Jespers-MacBook-Pro.local>
2016-08-08 21:59:54 -0700

    added some code for pin code test and private methods

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,14 +5,26 @@ class Atm
     @balance = 1000
   end
 
-  def withdraw(amount, account)
+  def withdraw(amount, pin_code, account)
     case
     when insufficient_funds_in_account?(amount, account) then
-      return
+      { status: false, message: 'insufficient funds in account', date: Date.today }
+    when insufficient_funds_in_atm?(amount) then
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    when incorrect_pin?(pin_code, account.pin_code) then
+      { status: false, message: 'wrong pin code', date: Date.today }
     else
       perform_transaction(amount, account)
     end
   end
+
+  def perform_transaction(amount, account)
+    @balance -= amount
+    account.balance -= amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
+  #private methods
 
   private
 
@@ -20,9 +32,12 @@ class Atm
     amount > account.balance
   end
 
-  def perform_transaction(amount, account)
-    @balance -= amount
-    account.balance -= amount
-    { status: true, message: 'success', date: Date.today, amount: amount }
+
+  def insufficient_funds_in_atm?(amount)
+    @balance < amount
+  end
+
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
   end
 end
