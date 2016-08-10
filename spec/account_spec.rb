0ac34242f2d224ce368e17fb6a9e77fdcc8e3ee3
JesperGreen <jesper@Jespers-MacBook-Pro.local>
2016-08-09 22:16:55 -0700

    added 4 digit randomized pin code

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,9 @@
 require './lib/account.rb'
+
+describe Account do
+
+  it 'is expected to have a 4 digit pin code on initialize' do
+    pin_length = Math.log10(subject.pin_code).to_i + 1
+    expect(pin_length).to eq 4
+  end
+end
