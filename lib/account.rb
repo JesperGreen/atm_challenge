2de3444992c4cbf41e9cb68315b5dfb81dafc683
JesperGreen <jesper@Jespers-MacBook-Pro.local>
2016-08-10 14:10:19 -0700

    added account status - active and deactivated

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,19 @@
 class Account
-  attr_accessor :pin_code, :balance
+  attr_accessor :pin_code, :balance, :account_status
 
   def initialize
     @pin_code = generate_pin
     @balance = 0
+    @account_status = :active
   end
 
+  def deactivate
+    @account_status = :deactivated
+  end
 
  ############### private methods ###############
   private
+
   def generate_pin
     rand(1000..9999)
   end
