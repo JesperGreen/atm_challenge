9f1f99a4cefacd42be8a0f8302a20eb583d9dfea
JesperGreen <jesper@Jespers-MacBook-Pro.local>
2016-08-10 13:54:44 -0700

    added balance of 0 on initialize

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,8 @@ describe Account do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
   end
+
+  it 'is expeted to have a balance of 0 on initialize' do
+    expect(subject.balance).to eq 0
+  end
 end
