b9ab712a9dbc92c23259066aa61d653308211f2e
JesperGreen <jesper@Jespers-MacBook-Pro.local>
2016-08-11 23:41:24 -0700

    integrating all parts

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,26 @@
 require 'account'
+require 'date'
 
 class Person
   attr_accessor :name, :cash, :account
 
   def initialize(attrs = {})
     @cash = 0
-    set_name(attrs[:name])
+    @name = set_name(attrs[:name])
   end
 
   def create_account
     @account = Account.new({ owner: self })
   end
 
-
   def deposit(amount)
-    @account == nil ?  missing_account : @account.balance += amount
+    @account == nil ? missing_account : deposit_funds(amount)
   end
+
+  def get_cash_from_atm(args = {})
+    @account == nil ? missing_account : withdraw_funds(args)
+  end
+
 ############### private methods ###############
 
 private
@@ -24,11 +29,33 @@ private
     obj == nil ?  missing_name : @name = obj
   end
 
+  def increase_cash(response)
+    @cash += response[:amount]
+  end
+
+  def withdraw_funds(args)
+    args[:atm] == nil ? missing_atm : atm = args[:atm]
+    account = @account
+    amount = args[:amount]
+    pin = args[:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] ? increase_cash(response) : response
+  end
+
+  def deposit_funds(amount)
+    @cash -= amount
+    @account.balance += amount
+  end
+
   def missing_name
     raise "A name is required"
   end
 
   def missing_account
     raise "No account present"
+  end
+
+  def missing_atm
+    raise "An ATM is required"
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -35,8 +35,40 @@ describe Person do
   describe 'can manage funds if an account been created' do
     let(:atm) { Atm.new }
     before { subject.create_account }
+
     it 'can deposit funds' do
       expect(subject.deposit(100)).to be_truthy
+    end
+
+    it 'funds are added to the accounst balance - deducted from cash' do
+      subject.cash = 100
+      subject.deposit(100)
+      expect(subject.account.balance).to be 100
+      expect(subject.cash).to be 0
+    end
+
+    it 'can withdraw funds' do
+      command = lambda { subject.get_cash_from_atm(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+      expect(command.call).to be_truthy
+    end
+
+    it 'withdraw is expected to raise error if no ATM is passed in' do
+      command = lambda { subject.get_cash_from_atm(amount: 100,
+        pin: subject.account.pin_code,
+        account: subject.account) }
+      expect { command.call }.to raise_error 'An ATM is required'
+    end
+
+    it 'funds are added to cash - deducted from account balance' do
+      allow(subject.account).to  receive(:exp_date).and_return('12/20')
+      subject.cash = 100
+      subject.deposit(100)
+      subject.get_cash_from_atm(amount: 100,
+      pin: subject.account.pin_code,
+      account: subject.account,
+      atm: atm)
+      expect(subject.account.balance).to be 0
+      expect(subject.cash).to be 100
     end
   end
 
